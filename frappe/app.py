e23fb7efade4893194dbbb11a19592da5c801b92
CypressAutumn <43881977+CypressAutumn@users.noreply.github.com>
2020-02-26 10:27:24 +0800

    Update app.py

--- frappe/app.py
+++ frappe/app.py
@@ -10,7 +10,7 @@ import logging
 from werkzeug.wrappers import Request
 from werkzeug.local import LocalManager
 from werkzeug.exceptions import HTTPException, NotFound
-from werkzeug.contrib.profiler import ProfilerMiddleware
+from werkzeug.middleware.profiler import ProfilerMiddleware
 from werkzeug.wsgi import SharedDataMiddleware
 
 import frappe
